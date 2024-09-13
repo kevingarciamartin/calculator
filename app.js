8c800c1fd1a684baabe6fd749724c47d6370f2f0
Kevin Garcia Martin <kevingm@live.se>
2024-09-13 10:48:25 +0200

    Add numbers to screen when buttons are clicked

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-let num1;
-let num2;
-let operator;
+let num1 = null;
+let num2 = null;
+let operator = null;
+
+const numButtons = document.querySelectorAll(".num-btn");
+
+numButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        populateDisplay(button.value);
+    });
+});
+
+function populateDisplay(value) {
+    const display = document.querySelector(".currentDisplay");
+    
+    if (operator === null) num1 = num1 === null ? value : num1 += value;
+    else num2 = num2 === null ? value : num2+= value;
+
+    display.textContent = `${num1}`;
+}
 
 function add(num1, num2) {
     return num1 + num2;
