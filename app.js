008d3458d46559b11c09e92dca198f5ba9bae365
Kevin Garcia Martin <kevingm@live.se>
2024-09-13 11:16:34 +0200

    Add event listeners for operator buttons

--- app.js
+++ app.js
@@ -2,37 +2,36 @@ let num1 = null;
 let num2 = null;
 let operator = null;
 
-const numButtons = document.querySelectorAll(".num-btn");
+const numberButtons = document.querySelectorAll(".num-btn");
+const operatorButtons = document.querySelectorAll(".operator-btn");
 
-numButtons.forEach((button) => {
+numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
         populateDisplay(button.value);
     });
 });
 
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        operator = button.value;
+        console.log(button.value);
+    });
+});
+
 function populateDisplay(value) {
     const display = document.querySelector(".currentDisplay");
+
+    let displayValue;
     
-    if (operator === null) num1 = num1 === null ? value : num1 += value;
-    else num2 = num2 === null ? value : num2+= value;
+    if (operator === null) {
+        num1 = num1 === null ? value : num1 += value;
+        displayValue = num1;
+    } else {
+        num2 = num2 === null ? value : num2+= value;
+        displayValue = num2;
+    } 
 
-    display.textContent = `${num1}`;
-}
-
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
+    display.textContent = `${displayValue}`;
 }
 
 function operate(operator, num1, num2) {
@@ -49,4 +48,20 @@ function operate(operator, num1, num2) {
             console.log("Error: Invalid operator.");
             break;
     }
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
 }
